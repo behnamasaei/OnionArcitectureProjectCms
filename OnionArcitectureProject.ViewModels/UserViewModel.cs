92dda7d9bf84c3c05e048476d3ce97d194329ae2
behnamasaei <behnamasaei@gmail.com>
2021-11-13 11:44:10 +0330

    add setting contorller and action List User

---
+++ OnionArcitectureProject.ViewModels/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnionArcitectureProject.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public string Role { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ OnionArcitectureProject.Web/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnionArcitectureProject.Entities;
+using OnionArcitectureProject.ViewModels;
+
+namespace OnionArcitectureProject.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles ="Admin , SuperAdmin")]
+    public class SettingController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SettingController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListUserAsync()
+        {
+            var users = _userManager.Users.ToList();
+            List<UserViewModel> usersInfo = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                usersInfo.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    UserName = user.UserName,
+                    Roles = await _userManager.GetRolesAsync(user)
+                });
+            }
+            return View(usersInfo);
+        }
+    }
+}
